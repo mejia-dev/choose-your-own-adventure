ebcad5a320808d0399e6f109b7212894fc1fa479
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-18 11:12:04 -0800

    Add button for initial push of data
    Co-authored-by: Teddy Peterschmidt <Teddyp14@users.noreply.github.com>
    Co-authored-by: Brianca Knight <BriancaKnight@users.noreply.github.com>

--- src/components/FirestoreControl.js
+++ src/components/FirestoreControl.js
@@ -1,61 +1,41 @@
+import React from "react";
 import { dungeonData, choiceData, enemiesData, lootData } from "./RawDungeonData";
 import db from "../firebase.js";
 import { collection, addDoc, setDoc, onSnapshot, doc, updateDoc } from "firebase/firestore";
 
-function FireStoreControl() {
+export default function FireStoreControl() {
 
-  const resetDataToDefault = async () => {
-    onSnapshot(collection(db, "dungeon"),
-      (collectionSnapshot) => {
-        collectionSnapshot.forEach(async(doc) => {
-          // find doc by id
-          // if doc in collection already matches related doc in RawDungeonData, do not update
-          // if doc does not match, update
-          if (doc != dungeonData[doc.id]) {
-            await setDoc(doc(db, "dungeon", doc.id), dungeonData[doc.id]);
-          }
-        })
-      },
-      (error) => {
-        console.log(error.message);
-      }
-    )
-    onSnapshot(collection(db, "choices"),
-      (collectionSnapshot) => {
-        collectionSnapshot.forEach(async(doc) => {
-          if (doc != choiceData[doc.id]) {
-            await setDoc(doc(db, "choices", doc.id), choiceData[doc.id]);
-          }
-        })
-      },
-      (error) => {
-        console.log(error.message);
-      }
-    )
-    onSnapshot(collection(db, "enemies"),
-      (collectionSnapshot) => {
-        collectionSnapshot.forEach(async(doc) => {
-          if (doc != enemiesData[doc.id]) {
-            await setDoc(doc(db, "enemies", doc.id), enemiesData[doc.id]);
-          }
-        })
-      },
-      (error) => {
-        console.log(error.message);
-      }
-    )
-    onSnapshot(collection(db, "loot"),
-      (collectionSnapshot) => {
-        collectionSnapshot.forEach(async(doc) => {
-          if (doc != lootData[doc.id]) {
-            await setDoc(doc(db, "loot", doc.id), lootData[doc.id]);
-          }
-        })
-      },
-      (error) => {
-        console.log(error.message);
-      }
-    )
+  const resetDefaultData = async () => {
+
+    dungeonData.forEach(async(object) => {
+        await setDoc(doc(db, "dungeon", object.id), object);
+      
+    })
+
+    choiceData.forEach(async(object) => {
+        await setDoc(doc(db, "choices", object.id), object);
+      
+    })
+
+    enemiesData.forEach(async(object) => {
+        await setDoc(doc(db, "enemies", object.id), object);
+      
+    })
+
+    lootData.forEach(async(object) => {
+        await setDoc(doc(db, "loot", object.id), object);
+      
+    })
   }
+
+  const testDataGenerator = async () => {
+    await setDoc(doc(db,"testCollection", "id"), { "test": "This is a test!"});
+  }
+
+  return (
+    <React.Fragment>
+        <button onClick={() => resetDefaultData()}>Reset</button>
+    </React.Fragment>
+  )
 
 }

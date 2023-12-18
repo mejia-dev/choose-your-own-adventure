94dff2f2c77ac74250f01ae0a50047499fcd78b7
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-18 10:21:16 -0800

    Create resetDataToDefault
    Co-authored-by: Teddy Peterschmidt <Teddyp14@users.noreply.github.com>
    Co-authored-by: Brianca Knight <BriancaKnight@users.noreply.github.com>

--- src/components/FirestoreControl.js
+++ src/components/FirestoreControl.js
@@ -1,3 +1,61 @@
 import { dungeonData, choiceData, enemiesData, lootData } from "./RawDungeonData";
 import db from "../firebase.js";
 import { collection, addDoc, setDoc, onSnapshot, doc, updateDoc } from "firebase/firestore";
+
+function FireStoreControl() {
+
+  const resetDataToDefault = async () => {
+    onSnapshot(collection(db, "dungeon"),
+      (collectionSnapshot) => {
+        collectionSnapshot.forEach(async(doc) => {
+          // find doc by id
+          // if doc in collection already matches related doc in RawDungeonData, do not update
+          // if doc does not match, update
+          if (doc != dungeonData[doc.id]) {
+            await setDoc(doc(db, "dungeon", doc.id), dungeonData[doc.id]);
+          }
+        })
+      },
+      (error) => {
+        console.log(error.message);
+      }
+    )
+    onSnapshot(collection(db, "choices"),
+      (collectionSnapshot) => {
+        collectionSnapshot.forEach(async(doc) => {
+          if (doc != choiceData[doc.id]) {
+            await setDoc(doc(db, "choices", doc.id), choiceData[doc.id]);
+          }
+        })
+      },
+      (error) => {
+        console.log(error.message);
+      }
+    )
+    onSnapshot(collection(db, "enemies"),
+      (collectionSnapshot) => {
+        collectionSnapshot.forEach(async(doc) => {
+          if (doc != enemiesData[doc.id]) {
+            await setDoc(doc(db, "enemies", doc.id), enemiesData[doc.id]);
+          }
+        })
+      },
+      (error) => {
+        console.log(error.message);
+      }
+    )
+    onSnapshot(collection(db, "loot"),
+      (collectionSnapshot) => {
+        collectionSnapshot.forEach(async(doc) => {
+          if (doc != lootData[doc.id]) {
+            await setDoc(doc(db, "loot", doc.id), lootData[doc.id]);
+          }
+        })
+      },
+      (error) => {
+        console.log(error.message);
+      }
+    )
+  }
+
+}

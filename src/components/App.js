e06fdf59c9af5baaf7ee5fe828de1ebd75144a2c
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-18 15:50:51 -0800

    Add SignIn page and have App render it

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,62 @@
+import React, { useState } from "react";
 import './App.css';
 import FirestoreControl from "./FirestoreControl"
+import { db, auth } from "../firebase";
+import SignIn from './SignIn';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import UIHolder from "./UIHolder";
+import { signOut } from "firebase/auth";
+
+// function doSignOut() {
+//   signOut(auth)
+//       .then(function() {
+//           setSignOutSuccess('You have successfully signed out!');
+//       }).catch(function(error) {
+//           setSignOutSuccess(`There was an error signing out: ${error.message}!`);
+//       });
+// }
 
 function App() {
+
+  const [signOutSuccess, setSignOutSuccess] = useState(null);
+  const [pageView, setPageView] = useState(0)
+
+  function doSignOut() {
+    signOut(auth)
+        .then(function() {
+            setSignOutSuccess('You have successfully signed out!');
+        }).catch(function(error) {
+            setSignOutSuccess(`There was an error signing out: ${error.message}!`);
+        });
+  }
+
+  let currentView;
+
+  if (!pageView) {
+    currentView = 
+    <Router>
+      <div>
+        <Link to="/">Home</Link><br />
+        <Link to="/sign-in">Sign In</Link>
+        <button onClick={doSignOut}>Sign out</button>
+        {signOutSuccess}
+      </div>
+      
+      <Routes>
+        <Route path='/sign-in' element={<SignIn changePage={setPageView}/>} />
+        <Route path='/' element={<UIHolder />} />
+      </Routes>
+    </Router>
+  } else {
+    currentView = 
+    <UIHolder />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <FirestoreControl />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      {currentView}
+    </React.Fragment>
+  )
 }
 
 export default App;
---
+++ src/components/SignIn.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { useState } from 'react';
+import { auth } from './../firebase';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import PropTypes from "prop-types";
+
+function SignIn(props) {
+
+    const [signUpSuccess, setSignUpSuccess] = useState(null);
+    const [signInSuccess, setSignInSuccess] = useState(null);
+    
+    function doSignUp(event) {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                setSignUpSuccess(`You've successfully signed up, ${userCredential.user.email}!`)
+            })
+            .catch((error) => {
+                setSignUpSuccess(`There was an error signing up: ${error.message}`)
+            });
+    }
+
+    function doSignIn(event) {
+        event.preventDefault();
+        const email = event.target.signinEmail.value;
+        const password = event.target.signinPassword.value;
+        signInWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+            setSignInSuccess(`You've successfully signed in as ${userCredential.user.email}!`);
+            props.changePage(1);
+          })
+          .catch((error) => {
+            setSignInSuccess(`There was an error signing in: ${error.message}!`)
+          });
+      }
+
+    return (
+        <React.Fragment>
+      <h1>Sign up</h1>
+      {signUpSuccess}
+      <form onSubmit={doSignUp}>
+        <input
+          type='text'
+          name='email'
+          placeholder='email' />
+        <input
+          type='password'
+          name='password'
+          placeholder='Password' />
+        <button type='submit'>Sign up</button>
+      </form>
+
+      <h1>Sign In</h1>
+      {signInSuccess}
+      <form onSubmit={doSignIn}>
+        <input
+          type='text'
+          name='signinEmail'
+          placeholder='email' />
+        <input
+          type='password'
+          name='signinPassword'
+          placeholder='Password' />
+        <button type='submit'>Sign in</button>
+      </form>
+      <br />
+      {/* <button onClick={doSignOut}>Sign out</button> */}
+    </React.Fragment>
+  );
+}
+
+SignIn.propTypes = {
+  changePage: PropTypes.func
+}
+
+export default SignIn

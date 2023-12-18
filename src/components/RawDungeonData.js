905a41780ee4bddd6a4a4976bf8d156199e00dc4
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-18 09:30:29 -0800

    Structure data
    Co-authored-by: Teddy Peterschmidt <Teddyp14@users.noreply.github.com>
    Co-authored-by: Brianca Knight <BriancaKnight@users.noreply.github.com>

---
+++ src/components/RawDungeonData.js
@@ -0,0 +1,120 @@
+const dungeonData = [
+  {
+    name: "Room 1",
+    choiceArray: [],
+    enemies: 1,
+    loot: 1,
+    text: "",
+  },
+  {
+    name: "Room 2",
+    choiceArray: [],
+    enemies: 2,
+    loot: 2,
+    text: "",
+  },
+  {
+    name: "Room 3",
+    choiceArray: [],
+    enemies: 3,
+    loot: 3,
+    text: "",
+  }
+]
+
+const choiceData = [
+  {
+    text: "",
+    buttonText: "",
+    resultText: "",
+    resultAction: {} // do something
+  },
+  {
+    text: "",
+    buttonText: "",
+    resultText: "",
+    resultAction: {} // do something
+  },
+  {
+    text: "",
+    buttonText: "",
+    resultText: "",
+    resultAction: {} // do something
+  },
+  {
+    text: "",
+    buttonText: "",
+    resultText: "",
+    resultAction: {} // do something
+  },
+  {
+    text: "",
+    buttonText: "",
+    resultText: "",
+    resultAction: {} // do something
+  },
+  {
+    text: "",
+    buttonText: "",
+    resultText: "",
+    resultAction: {} // do something
+  },
+]
+
+const enemiesData = [
+  {
+    type: "",
+    health: 0,
+    strength: 0,
+    defense: 0
+  },
+  {
+    type: "",
+    health: 0,
+    strength: 0,
+    defense: 0
+  },
+  {
+    type: "",
+    health: 0,
+    strength: 0,
+    defense: 0
+  },
+]
+
+const lootData = [
+  {
+    obtainText: "",
+    itemDescription: "",
+    type: "",
+    health: 0
+  },
+  {
+    obtainText: "",
+    itemDescription: "",
+    type: "",
+    strength: 0
+  },
+  {
+    obtainText: "",
+    itemDescription: "",
+    type: "",
+    money: 0
+  },
+]
+
+// {
+//   name: "Enchantments Trail",
+//   region: "Enchantments",
+//   description: "23.6 mile trail through Enchantments near Leavenworth, WA.",
+//   reviews: {
+//     1: {
+//       content: "Amazing!"
+//     },
+//     2: {
+//       content: "Very challenging hike."
+//     },
+//   }
+// }
+
+

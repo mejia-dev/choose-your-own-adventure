3310ec0d231861db6072a276fdd2dbe67ff2b258
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-19 14:27:46 -0800

    Clean up code
    Co-authored-by: Brianca Knight <BriancaKnight@users.noreply.github.com>
    Co-authored-by: Teddy Peterschmidt <Teddyp14@users.noreply.github.com>

--- src/components/App.js
+++ src/components/App.js
@@ -7,15 +7,6 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import UIHolder from "./UIHolder";
 import { signOut } from "firebase/auth";
 
-// function doSignOut() {
-//   signOut(auth)
-//       .then(function() {
-//           setSignOutSuccess('You have successfully signed out!');
-//       }).catch(function(error) {
-//           setSignOutSuccess(`There was an error signing out: ${error.message}!`);
-//       });
-// }
-
 function App() {
 
   const [signOutSuccess, setSignOutSuccess] = useState(null);
@@ -23,35 +14,35 @@ function App() {
 
   function doSignOut() {
     signOut(auth)
-        .then(function() {
-            setSignOutSuccess('You have successfully signed out!');
-        }).catch(function(error) {
-            setSignOutSuccess(`There was an error signing out: ${error.message}!`);
-        });
+      .then(function () {
+        setSignOutSuccess('You have successfully signed out!');
+      }).catch(function (error) {
+        setSignOutSuccess(`There was an error signing out: ${error.message}!`);
+      });
   }
 
   let currentView;
 
   if (!pageView) {
-    currentView = 
-    <Router>
-      <div>
-        <Link to="/">Home</Link><br />
-        <Link to="/sign-in">Sign In</Link>
-        <button onClick={doSignOut}>Sign out</button>
-        {signOutSuccess}
-      </div>
-      
-      <Routes>
-        <Route path='/sign-in' element={<SignIn 
-        changePage={setPageView}
-        />} />
-        <Route path='/' element={<UIHolder />} />
-      </Routes>
-    </Router>
+    currentView =
+      <Router>
+        <div>
+          <Link to="/">Home</Link><br />
+          <Link to="/sign-in">Sign In</Link>
+          <button onClick={doSignOut}>Sign out</button>
+          {signOutSuccess}
+        </div>
+
+        <Routes>
+          <Route path='/sign-in' element={<SignIn
+            changePage={setPageView}
+          />} />
+          <Route path='/' element={<UIHolder />} />
+        </Routes>
+      </Router>
   } else {
-    currentView = 
-    <UIHolder />
+    currentView =
+      <UIHolder />
   }
 
   return (

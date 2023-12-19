397428d37998d44d22d1e33a8f9dd1792412997f
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-18 16:02:40 -0800

    Add PlayerInput..js
    Co-authored-by: Brianca Knight <BriancaKnight@users.noreply.github.com>
    Co-authored-by: Teddy Peterschmidt <Teddyp14@users.noreply.github.com>

---
+++ src/components/PlayerInput.js
@@ -0,0 +1,12 @@
+const PlayerInput = () => {
+    return (
+        <form>
+            <label> Ahoy! What be yer name!?
+                <input type='text' id='player-name'/>
+            </label>
+            <button type='submit'>Arrrrrg!</button>
+        </form>
+    )
+};
+
+export default PlayerInput;

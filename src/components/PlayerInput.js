e888cd46b01a1710598961a22038937b4ce2874c
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-19 13:09:00 -0800

    Finalize playerInput form
    Co-authored-by: Brianca Knight <BriancaKnight@users.noreply.github.com>
    Co-authored-by: Teddy Peterschmidt <Teddyp14@users.noreply.github.com>

--- src/components/PlayerInput.js
+++ src/components/PlayerInput.js
@@ -1,9 +1,25 @@
+import playerData from './PlayerData'
+
 const PlayerInput = () => {
+
+    const handleInput = (e) => {
+        playerData.name = e.target.value
+        console.log(playerData.name)
+    }
+
+    const preventRefresh = (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <form>
+        <form onSubmit={preventRefresh}>
+            
             <label> Ahoy! What be yer name!?
-                <input type='text' id='player-name'/>
+                <input type='text' 
+                id='player-name'
+                onChange={handleInput}/>
             </label>
+
             <button type='submit'>Arrrrrg!</button>
         </form>
     )

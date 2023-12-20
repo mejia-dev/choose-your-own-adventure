7255b37ba96c5adac8ba6496f6656c19c3e20f1a
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-20 09:48:23 -0800

    Update Ship to give the player an inventory item
    Co-authored-by: Brianca Knight <BriancaKnight@users.noreply.github.com>
    Co-authored-by: Teddy Peterschmidt <Teddyp14@users.noreply.github.com>

--- src/components/Ship.js
+++ src/components/Ship.js
@@ -1,44 +1,88 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import { query, collection, where, getDocs } from 'firebase/firestore';
+import { db } from './../firebase.js';
+import playerData from "./PlayerData.js";
 
 export default function Ship(props) {
   const styleHidden = {
     display: "hidden"
   }
-  
+
   const [button1Style, setButton1Style] = useState({});
   const [button2Style, setButton2Style] = useState({});
   const [resultTextStyle, setResultTextStyle] = useState(styleHidden);
-  
-    let resultText;
+  const [resultText, setResultText] = useState(null);
+  const [lootedItemDisplay, setLootedItemDisplay] = useState(null);
 
-    function result1() {
-        resultText = props.selectedChoices[0].resultText;
-        setButton2Style(styleHidden);
-        setResultTextStyle(null);
-        props.commitChoice()
-    }
+  let currentView;
 
-    function result2() {
-        resultText = props.selectedChoices[1].resultText;
-        setButton1Style(styleHidden);
-        setResultTextStyle(null);
-        props.commitChoice()
-    }
-
-    return(
-        <>
-            <h2>You've <b>arr</b>ived aboard the {props.selectedShip.name}</h2>
-            <p>{props.selectedShip.storyText}</p>
-            <div style={button1Style}><button onClick={() => result1()}>{props.selectedChoices[0].buttonText}</button></div>
-            <div style={button2Style}><button onClick={() => result2()}>{props.selectedChoices[1].buttonText}</button></div>
-            <p style={resultTextStyle}>{resultText}</p>
-        </>
+  if (!resultText) {
+    currentView = (
+      <>
+        <div style={button1Style}><button onClick={() => result1()}>{props.selectedChoices[0].buttonText}</button></div>
+        <div style={button2Style}><button onClick={() => result2()}>{props.selectedChoices[1].buttonText}</button></div>
+      </>
     )
+  } else {
+    currentView = (
+      <>
+        <h2>Test</h2>
+      </>
+    )
+  }
+
+  async function getLootedItem(lootID) {
+    const q = query(collection(db, "loot"), where("id", "==", lootID));
+    const querySnapshot = await getDocs(q);
+    setLootedItemDisplay(null)
+    querySnapshot.forEach((doc) => {
+      playerData.inventory.push(doc.data());
+      setLootedItemDisplay(
+        <>
+          <h4>{doc.data().type}</h4>
+          <p>{doc.data().obtainText}</p>
+          <p>{doc.data().itemDescription}</p>
+        </>
+      )
+    });
+    console.log(playerData.inventory)
+  }
+
+
+  function result1() {
+    setResultText(props.selectedChoices[0].resultText)
+    setButton2Style(styleHidden);
+    setResultTextStyle(null);
+    getLootedItem(props.selectedChoices[0].loot);
+  }
+
+  function result2() {
+    setResultText(props.selectedChoices[1].resultText)
+    setButton1Style(styleHidden);
+    setResultTextStyle(null);
+    getLootedItem(props.selectedChoices[1].loot)
+  }
+
+
+  return (
+    <>
+      <h2>You've <b>arr</b>ived aboard the {props.selectedShip.name}</h2>
+      <p>{props.selectedShip.storyText}</p>
+      {currentView}
+      {/* <div style={button1Style}><button onClick={() => result1()}>{props.selectedChoices[0].buttonText}</button></div>
+            <div style={button2Style}><button onClick={() => result2()}>{props.selectedChoices[1].buttonText}</button></div> */}
+      <div style={resultTextStyle}>
+        {resultText}
+        <br /><br />
+        {lootedItemDisplay}
+      </div>
+    </>
+  )
 }
 
 Ship.propTypes = {
   selectedShip: PropTypes.object,
   selectedChoices: PropTypes.array,
-  commitChoice: PropTypes.func
+  // commitChoice: PropTypes.func
 }
